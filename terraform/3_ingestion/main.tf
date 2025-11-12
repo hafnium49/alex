f0da197ecbe2638b18daf2da6335e90f806be998
Hiroki Fujiwara <hafnium49@gmail.com>
2025-11-13 05:34:06 +0900

    fix: remove unnecessary whitespace in S3 bucket resource

--- terraform/3_ingestion/main.tf
+++ terraform/3_ingestion/main.tf
@@ -25,7 +25,7 @@ data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "vectors" {
   bucket = "alex-vectors-${data.aws_caller_identity.current.account_id}"
-  
+
   tags = {
     Project = "alex"
     Part    = "3"
